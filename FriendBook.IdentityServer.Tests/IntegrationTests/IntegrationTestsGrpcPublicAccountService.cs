76589f9db0037cbf3d810f926fda41848805e756
brikez <brike@DESKTOP-F74V8DB>
2023-07-28 19:18:27 +0300

    IntegrationTestsGrpcPublicAccountService inherit BaseIntegrationTests. Refactoring IntegrationTestsGrpcPublicAccountService.

--- FriendBook.IdentityServer.Tests/IntegrationTests/IntegrationTestsGrpcPublicAccountService.cs
+++ FriendBook.IdentityServer.Tests/IntegrationTests/IntegrationTestsGrpcPublicAccountService.cs
@@ -1,101 +1,43 @@
-﻿using FriendBook.IdentityServer.API.DAL;
-using FriendBook.IdentityServer.API.Domain.DTO.AccountsDTO;
-using FriendBook.IdentityServer.API.Domain.DTO;
-using FriendBook.IdentityServer.API.Domain.JWT;
-using FriendBook.IdentityServer.API.Domain.Settings;
+﻿using FriendBook.IdentityServer.API.Domain.DTO.AccountsDTO;
 using FriendBook.IdentityServer.Tests.IntegrationTests.IntegrationTestFixtureSources;
-using FriendBook.IdentityServer.Tests.WebAppFactories;
-using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
-using FriendBook.IdentityServer.API;
-using Microsoft.Extensions.DependencyInjection;
 using System.Net;
 using FriendBook.IdentityServer.API.BLL.gRPCServices.AccountService;
-using FriendBook.IdentityServer.API.Domain.Response;
 using FriendBook.IdentityServer.Tests.TestHelpers;
 
 namespace FriendBook.IdentityServer.Tests.IntegrationTests
 {
     [TestFixtureSource(typeof(IntegrationTestFixtureSource))]
-    public class IntegrationTestsGrpcPublicAccountService
+    public class IntegrationTestsGrpcPublicAccountService : BaseIntegrationTests
     {
-        private WebHostFactory<Program, IdentityContext> _webHost;
-        private HttpClient _httpClient;
+        internal const string UrlController = $"{UrlAPI}/GrpcAccountService";
 
-        private readonly RequestNewAccount _requestAccount;
-        private ResponseAuthenticate _responseRegistries;
-        private DataAccessToken _userData;
+        public IntegrationTestsGrpcPublicAccountService(RequestNewAccount requestAccount) : base(requestAccount){}
 
-        internal const string UrlController = "api/v1/GrpcAccountService";
-
-        public IntegrationTestsGrpcPublicAccountService(RequestNewAccount requestAccount)
-        {
-            _requestAccount = requestAccount;
-        }
-
-        [OneTimeSetUp]
-        public async Task Setup()
-        {
-            _webHost = new WebHostFactory<Program, IdentityContext>();
-            await _webHost.InitializeAsync();
-
-            _httpClient = _webHost.CreateClient();
-        }
-
-        [SetUp]
-        public async Task TestRegistrationTestAccount()
-        {
-            HttpContent requestAccountContent = JsonContent.Create(_requestAccount);
-            HttpResponseMessage responseAuth = await _httpClient.PostAsync($"{IntegrationTestsIdentityServerController.UrlController}/Registration", requestAccountContent);
-            _responseRegistries = (await DeserializeHelper.TryDeserializeStandartResponse<ResponseAuthenticate>(responseAuth)).Data 
-                ?? throw new InvalidOperationException($"Data ResponseAuthenticated is null");
-
-            var jwtSettings = _webHost.Services.GetService<IOptions<JWTSettings>>()?.Value
-                ?? throw new InvalidOperationException($"{JWTSettings.Name} not found");
-            _userData = TokenHelpers.GetDataTokenAuth(_responseRegistries.AccessToken, jwtSettings.Issuer, jwtSettings.Audience, jwtSettings.AccessTokenSecretKey)
-                ?? throw new InvalidOperationException("The access token cannot be read");
-
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _responseRegistries.AccessToken);
-        }
-        [TearDown]
-        public async Task Clear()
-        {
-            await _webHost.ClearData();
-        }
-
-        [OneTimeTearDown]
-        public async Task Dispose()
-        {
-            await _webHost.DisposeAsync();
-        }
         [Test]
         public async Task CheckUserExists() 
         {
-            HttpResponseMessage responseUserExists = await _httpClient.GetAsync($"{UrlController}/CheckUserExists?AccountId={_userData.Id}");
-            ResponseUserExists objResponseUserExists = JsonConvert.DeserializeObject<ResponseUserExists>(await responseUserExists.Content.ReadAsStringAsync())
-                ?? throw new JsonException("Error when parsing JSON: Response user exists");
+            HttpResponseMessage httpResponseUserExists = await _httpClient.GetAsync($"{UrlController}/CheckUserExists?AccountId={_userData.Id}");
+            var responseUserExists = await DeserializeHelper.TryDeserialize<ResponseUserExists>(httpResponseUserExists);
 
             Assert.Multiple(() =>
             {
-                Assert.That(responseUserExists.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                Assert.That(objResponseUserExists.Exists, Is.EqualTo(true));
+                Assert.That(httpResponseUserExists.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(responseUserExists.Exists, Is.EqualTo(true));
             });
         }
+
         [Test]
         public async Task GetUsersLoginById()
         {
-            HttpResponseMessage responseUsersLogin = await _httpClient.GetAsync($"{UrlController}/GetUsersLoginById?UserId={_userData.Id}&UserId={_userData.Id}");
-            ResponseUsers objResponseUsersLogin = JsonConvert.DeserializeObject<ResponseUsers>(await responseUsersLogin.Content.ReadAsStringAsync())
-                ?? throw new JsonException("Error when parsing JSON: Response users login");
+            HttpResponseMessage httpResponseUsersLogin = await _httpClient.GetAsync($"{UrlController}/GetUsersLoginById?UserId={_userData.Id}&UserId={_userData.Id}");
+            var responseUsersLogin = await DeserializeHelper.TryDeserialize<ResponseUsers>(httpResponseUsersLogin);
 
             Assert.Multiple(() =>
             {
-                Assert.That(responseUsersLogin.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                Assert.That(objResponseUsersLogin.Users.Count, Is.EqualTo(1));
-                Assert.That(objResponseUsersLogin.Users[0].Login, Is.EqualTo(_userData.Login));
-                Assert.That(objResponseUsersLogin.Users[0].Id, Is.EqualTo(_userData.Id.ToString()));
+                Assert.That(httpResponseUsersLogin.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(responseUsersLogin.Users.Count, Is.EqualTo(1));
+                Assert.That(responseUsersLogin.Users[0].Login, Is.EqualTo(_userData.Login));
+                Assert.That(responseUsersLogin.Users[0].Id, Is.EqualTo(_userData.Id.ToString()));
             });
         }
     }
--- FriendBook.IdentityServer.Tests/TestHelpers/DeserializeHelper.cs
+++ FriendBook.IdentityServer.Tests/TestHelpers/DeserializeHelper.cs
@@ -10,5 +10,10 @@ namespace FriendBook.IdentityServer.Tests.TestHelpers
             return JsonConvert.DeserializeObject<StandartResponse<T>>(await httpResponseMessage.Content.ReadAsStringAsync())
                 ?? throw new JsonException($"Error deserialize JSON: StandartResponse<{typeof(T)}>");
         }
+        internal static async Task<T> TryDeserialize<T>(HttpResponseMessage httpResponseMessage)
+        {
+            return JsonConvert.DeserializeObject<T>(await httpResponseMessage.Content.ReadAsStringAsync())
+                ?? throw new JsonException($"Error deserialize JSON: {typeof(T)}");
+        }
     }
 }
